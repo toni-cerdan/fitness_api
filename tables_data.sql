6fdbb1050b2d478f39030f25307a23d3e0e6c8c3
Toni <cerdanigualtoni@gmail.com>
2022-07-24 18:10:33 +0200

    chore: add data to categories and body parts sql tables

---
+++ tables_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO Categories (name)
+VALUES ('Barbell'),
+    ('Dumbbell'),
+    ('Kettlebell'),
+    ('Machine'),
+    ('Bodyweight'),
+    ('Assisted'),
+    ('Duration'),
+    ('Cardio'),
+    ('Other');
+INSERT INTO Body_parts (name)
+VALUES ('Chest'),
+    ('Shoulders'),
+    ('Biceps'),
+    ('Triceps'),
+    ('Forearms'),
+    ('Abs'),
+    ('Legs'),
+    ('Calves'),
+    ('Quads'),
+    ('Hamstrings'),
+    ('Glutes'),
+    ('All'),
+    ('Other');
