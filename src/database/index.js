7b3bb226004a3729ae198a4f9278228938a5b336
Toni <cerdanigualtoni@gmail.com>
2022-07-27 20:59:46 +0200

    refact: move postgres database config file

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,16 @@
-const pg = require('pg');
+const { Pool } = require('pg');
 
-const dbConfig = {
+const pool = new Pool({
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    max: 10,
-    idleTimeoutMillis: 30000
-};
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+});
 
-const pool = new pg.Pool(dbConfig);
 pool.on('error', (err) => {
     console.log('idle client error', err.message, err.stack);
 });
