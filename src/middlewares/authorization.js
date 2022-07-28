71b257b01e000baf6bbefd5b83f4085188cd7f21
Toni <cerdanigualtoni@gmail.com>
2022-07-28 20:24:01 +0200

    refact: authorization middleware

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,14 +1,18 @@
-module.exports = {
-    isAuthorized: (req, res, next) => {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.status(401).json({ status: 'NOK', message: 'Unauthorized' });
-    },
-    isNotLoggedOut: (req, res, next) => {
-        if (req.isAuthenticated()) {
-            return next();
-        }
-        res.status(400).json({ status: 'NOK', message: 'Already logged out' });
+const isAuthorized = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
     }
+    res.status(401).json({ status: 'NOK', message: 'Unauthorized' });
+}
+
+const isNotLoggedOut = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.status(400).json({ status: 'NOK', message: 'Already logged out' });
+}
+
+module.exports = {
+    isAuthorized,
+    isNotLoggedOut
 }
