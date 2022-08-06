4ffb1efd138e8967ab08031bb5ae13bb4e634eae
Toni <cerdanigualtoni@gmail.com>
2022-08-07 00:11:34 +0200

    fix: no exercises found response error

--- src/controllers/exerciseController.js
+++ src/controllers/exerciseController.js
@@ -4,10 +4,17 @@ const getExercises = (req, res) => {
     const { category, body_part } = req.query;
     exerciseService.getExercises(category, body_part)
         .then(exercises => {
-            res.json({
-                status: 'OK',
-                data: exercises
-            });
+            if (exercises.length === 0) {
+                res.status(404).json({
+                    status: 'NOK',
+                    message: 'No exercises found'
+                });
+            } else {
+                res.json({
+                    status: 'OK',
+                    data: exercises
+                });
+            }
         }).catch(err => {
             res.status(404).json({
                 status: 'NOK',
